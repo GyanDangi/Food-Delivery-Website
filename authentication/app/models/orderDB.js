0648bcc21046a4edd528a77678a654153875b56e
Anurag <anuragmishra4499@gmail.com>
2022-11-06 21:43:18 +0530

    first commit

---
+++ authentication/app/models/orderDB.js
@@ -0,0 +1,12 @@
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+// detailSchema1 = new Schema( {
+// 	p_name: String,
+// 	p_category: String,
+// 	p_price: String,
+// 	p_image: String
+// }),
+// Detail1 = mongoose.model('food', detailSchema1);
+
+// module.exports = Detail1;
